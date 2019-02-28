cd25ea6f26349a0b86fe709688af7c10e0042403
Tobe O <thosakwe@gmail.com>
2019-02-28 01:05:36 -0500

    Add activation

--- day4_feedforward_network/layer.py
+++ day4_feedforward_network/layer.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 class Layer:
-    def __init__(self, input_shape):
+    def __init__(self, activation, input_shape):
+        self.activation = activation
         self.input_shape = input_shape
         self.weights = np.random.uniform(size=input_shape)
